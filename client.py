f2beee9c0b6e4bb0efea851c41ae468cc163839b
sumitgadwal <158582872+sumitgadwal@users.noreply.github.com>
2024-08-25 01:38:01 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,55 +1,51 @@
-# client_gui.py
-
-import sys
-import cv2
-import numpy as np
 import socket
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
-from PyQt5.QtCore import QTimer
+import tkinter as tk
+from PIL import ImageGrab
+import io
 
-class ClientApp(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.timer = QTimer()
+class ClientApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title('Client - Screen Sender')
 
-    def initUI(self):
-        self.setWindowTitle('Client - Screen Sender')
-        self.setGeometry(100, 100, 300, 100)
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect(('192.168.200.27', 9999))
 
-        self.start_btn = QPushButton('Start Sharing', self)
-        self.start_btn.clicked.connect(self.start_sharing)
+        self.start_button = tk.Button(master, text='Start Sharing', command=self.start_sharing)
+        self.start_button.pack()
 
-        self.stop_btn = QPushButton('Stop Sharing', self)
-        self.stop_btn.clicked.connect(self.stop_sharing)
-        self.stop_btn.setEnabled(False)
+        self.stop_button = tk.Button(master, text='Stop Sharing', command=self.stop_sharing)
+        self.stop_button.pack()
+        self.stop_button.config(state='disabled')
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.start_btn)
-        layout.addWidget(self.stop_btn)
-
-        self.setLayout(layout)
+        self.sending = False
 
     def start_sharing(self):
-        self.timer.timeout.connect(self.send_screen)
-        self.timer.start(30)  # Send a frame every 30ms
-
-        self.start_btn.setEnabled(False)
-        self.stop_btn.setEnabled(True)
+        self.sending = True
+        self.start_button.config(state='disabled')
+        self.stop_button.config(state='normal')
+        self.send_screen()
 
     def send_screen(self):
-        screen = np.array(cv2.cvtColor(np.array(cv2.VideoCapture(0).read()[1]), cv2.COLOR_BGR2RGB))
-        _, buffer = cv2.imencode('.jpg', screen, [int(cv2.IMWRITE_JPEG_QUALITY), 30])
-        self.client_socket.sendto(buffer, ('192.168.200.27', 9999))
+        if self.sending:
+            screen = ImageGrab.grab()
+            buffer = io.BytesIO()
+            screen.save(buffer, format='JPEG')
+            image_data = buffer.getvalue()
+
+            self.client_socket.send(len(image_data).to_bytes(4, 'big'))
+            self.client_socket.sendall(image_data)
+
+            self.master.after(30, self.send_screen)  # Send every 30 ms
 
     def stop_sharing(self):
-        self.timer.stop()
-        self.start_btn.setEnabled(True)
-        self.stop_btn.setEnabled(False)
+        self.sending = False
+        self.start_button.config(state='normal')
+        self.stop_button.config(state='disabled')
+
+        self.client_socket.close()
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = ClientApp()
-    ex.show()
-    sys.exit(app.exec_())
+    root = tk.Tk()
+    app = ClientApp(root)
+    root.mainloop()
